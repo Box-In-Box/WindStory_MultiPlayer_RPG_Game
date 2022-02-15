279879249970b20c293af63022a7daeefa938aa2
Baewon Lee <dgf0000@naver.com>
2022-02-15 19:27:45 +0900

    fix: Overall
    fix: map, ump, RPCmove, downjump, upjump...

--- WindStory/Assets/Scripts/PlayerController.cs
+++ WindStory/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ using Cinemachine;
 
 /*
  * 
- * XXX 속도가 내려갈때 가속되면 콜라이더 뚫고 내려가버림
+ * fixed 속도가 내려갈때 가속되면 콜라이더 뚫고 내려가버림
+ * 내 방향 앞에 타일이 있고 앞에밑에 타일이 있으면 rigid kinematic
+ * 점프중에는 레이어 변경하면 안됨 콜라이더트리거 하나 더만들어서 (몸(상체))이안들어가있고 isground이면 착지
  * 
  */
 public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
@@ -17,10 +19,12 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     public Animator anim;
     public PhotonView PV;
     public TransferMap theTransfer;
+    public Transform groundCheckCollider;
 
     [Header ("---Setting---")]
-    [SerializeField] [Range(1f, 10f)] float speed = 5f;
-    [SerializeField] [Range(100f, 500f)] float jumpForce = 100f;
+    [SerializeField] [Range(1f, 10f)] float speed;
+    [SerializeField] [Range(100f, 1000f)] float jumpForce;
+    [SerializeField] [Range(0f, 1f)] float checkGroundDistance = 0.3f;
 
     [Header ("---Check---")]
     public bool isFacingRight;
@@ -28,21 +32,20 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     public bool isUpGround;
     public bool doubleJumpState;
     public bool isMove;
-
-    [Header("---Camera---")]
-    public static CinemachineVirtualCamera chinemaCamera;
-    public static CinemachineConfiner chinemaConfiner;
-    public Collider2D currentChinemaCollider;
-
+    private bool isJump;
+    
     [Header("---Sound---")]
     public string walkSound;
     public bool timeCheck = true;
 
     [Header("---MAP---")]
+    public Collider2D currentChinemaCollider;
     public GameObject transfer;
     public string currentMapName;
     public string targetMapName;
     public bool isInTransferObj;
+    public static CinemachineVirtualCamera chinemaCamera;
+    public static CinemachineConfiner chinemaConfiner;
 
     [Header("---ETC---")]
     public string npcName;
@@ -53,9 +56,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     private AudioManager theAudio;
     private NpcScript theNpc;
 
+    private float axis;
     Vector3 curPos;
-
-    int playerLayer, passplayerLayer, passgroundLayer;   //레이어통과
+    int playerLayer, groundPlayerLayer, passPlayerLayer, groundLayer, passGroundLayer;
 
     void Awake()
     {
@@ -70,10 +73,12 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 
             //레이어 통과
             playerLayer = LayerMask.NameToLayer("Player");
-            passplayerLayer = LayerMask.NameToLayer("PassPlayer");
-            passgroundLayer = LayerMask.NameToLayer("PassGround"); 
+            groundPlayerLayer = LayerMask.NameToLayer("GroundPlayer");
+            passPlayerLayer = LayerMask.NameToLayer("PassPlayer");
+            groundLayer = LayerMask.NameToLayer("Ground");
+            passGroundLayer = LayerMask.NameToLayer("PassGround"); 
             Physics2D.IgnoreLayerCollision(playerLayer, playerLayer, true);
-            Physics2D.IgnoreLayerCollision(playerLayer, passplayerLayer, true);
+            Physics2D.IgnoreLayerCollision(playerLayer, passPlayerLayer, true);
         }
         //닉네임
         transform.Find("Canvas").gameObject.transform.Find("NickNameText").gameObject.GetComponent<Text>().text = PV.IsMine ? PhotonNetwork.LocalPlayer.NickName : PV.Owner.NickName;
@@ -90,10 +95,7 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (PV.IsMine)
         {
-            //← → 이동
-            float axis = Input.GetAxisRaw("Horizontal");
-            rigid.velocity = new Vector2(speed * axis, rigid.velocity.y);
-
+            axis = Input.GetAxisRaw("Horizontal");
             if ((axis > 0 && isFacingRight == false) || (axis < 0 && isFacingRight == true))
             {
                 PV.RPC("FlipXRPC", RpcTarget.AllBuffered);
@@ -113,38 +115,35 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
             }
 
             //바닥체크
-            isGround = Physics2D.OverlapCircle((Vector2)transform.position, 0.3f, 1 << LayerMask.NameToLayer("Ground"));
-            isUpGround = Physics2D.OverlapCircle((Vector2)transform.position, 0.3f, 1 << LayerMask.NameToLayer("PassGround"));
-            Debug.DrawRay(transform.position, new Vector2(0, 0.3f), Color.red);
+            isGround = Physics2D.OverlapCircle((Vector2)transform.position, checkGroundDistance, 1 << groundLayer);
+            isUpGround = Physics2D.OverlapCircle((Vector2)transform.position, checkGroundDistance, 1 << passGroundLayer);
+            Debug.DrawRay(transform.position, Vector2.down * checkGroundDistance, Color.green);
 
             //↑ 점프, ↑↑ 더블 점프
             if (isUpGround) isGround = true;
             if (isGround) doubleJumpState = true;
+
             PV.RPC("animationRPC", RpcTarget.AllBuffered, "isJump", !isGround);
-            if (Input.GetKeyDown(KeyCode.UpArrow) && isGround) PV.RPC("jumpRPC", RpcTarget.All);
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGround)
+            {
+                PV.RPC("jumpRPC", RpcTarget.All);
+            }
             else if (Input.GetKeyDown(KeyCode.UpArrow) && doubleJumpState)
             {
                 doubleJumpState = false;
                 PV.RPC("jumpRPC", RpcTarget.All);
             }
 
-            //↑ 레이어 위로통과
-            if (rigid.velocity.y > 0)
-                Physics2D.IgnoreLayerCollision(playerLayer, passgroundLayer, true);
-            else
-                Physics2D.IgnoreLayerCollision(playerLayer, passgroundLayer, false);
-
             //↓ 레이어 아래로통과
             if (isUpGround && Input.GetKeyDown(KeyCode.DownArrow))
             {
-                float curY = transform.position.y;
-                StartCoroutine("PassGround", curY);
+                StartCoroutine("PassDownGround", transform.position.y);
             }
             //↓ 맵이동
             if (isInTransferObj)
             {
                 targetMapName = transfer.gameObject.GetComponent<TransferMap>().targetPoint.parent.name;  //이동할 맵 표시
-                if(Input.GetKeyDown(KeyCode.DownArrow)) //맵 이동
+                if (Input.GetKeyDown(KeyCode.DownArrow)) //맵 이동
                 {
                     //카메라 설정
                     chinemaConfiner.m_BoundingShape2D = currentChinemaCollider;
@@ -152,9 +151,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 
                     //플레이어 카메라 설정
                     transform.position = transfer.gameObject.GetComponent<TransferMap>().targetPoint.transform.position;
-                    currentChinemaCollider = transfer.gameObject.GetComponent<TransferMap>().targetPoint.transform.parent.gameObject.transform.Find("CMRange_"+currentMapName).gameObject.GetComponent<Collider2D>();
+                    currentChinemaCollider = transfer.gameObject.GetComponent<TransferMap>().targetPoint.transform.parent.gameObject.transform.Find("CMRange_" + currentMapName).gameObject.GetComponent<Collider2D>();
                     chinemaConfiner.m_BoundingShape2D = currentChinemaCollider;
-                }   
+                }
             }
 
             //사운드
@@ -188,6 +187,30 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
         else transform.position = Vector3.Lerp(this.gameObject.transform.position, curPos, Time.deltaTime * 10);
     }
 
+    void Jump()
+    {
+        isJump = true;
+        if (PV.IsMine)
+        {
+            //↑ 레이어 위로통과
+            StopAllCoroutines();
+            StartCoroutine("PassUpGround");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //← → 이동
+        rigid.velocity = new Vector2(speed * axis, rigid.velocity.y);
+        if (isJump)
+        {
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(Vector2.up * jumpForce);
+            isJump = false;
+        }
+
+    }
+
     [PunRPC]    //좌우반전
     void FlipXRPC()
     {
@@ -204,8 +227,7 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     [PunRPC]    //점프
     void jumpRPC()
     {
-        rigid.velocity = Vector2.zero;
-        rigid.AddForce(Vector2.up * jumpForce);
+        Jump();
     }
     [PunRPC]    //사운드
     void soundRPC(string _soundName)
@@ -213,14 +235,23 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
         theAudio.Play(_soundName);
     }
 
-    //통과 코루틴
-    IEnumerator PassGround(float curY)
+    //↓ 레이어 아래로통과 코루틴
+    IEnumerator PassDownGround(float curY)
     {
-        Physics2D.IgnoreLayerCollision(passplayerLayer, passgroundLayer, true);
-        gameObject.layer = LayerMask.NameToLayer("PassPlayer");
-        yield return new WaitUntil(() => curY - transform.position.y > 0.2f);
-        Physics2D.IgnoreLayerCollision(passplayerLayer, passgroundLayer, false);
-        gameObject.layer = LayerMask.NameToLayer("Player");
+        Physics2D.IgnoreLayerCollision(passPlayerLayer, passGroundLayer, true);
+        gameObject.layer = passPlayerLayer;
+        yield return new WaitUntil(() => curY - transform.position.y > 0.3f);
+        Physics2D.IgnoreLayerCollision(passPlayerLayer, passGroundLayer, false);
+        gameObject.layer = playerLayer;
+    }
+    //↑ 레이어 위로통과 코루틴
+    IEnumerator PassUpGround()
+    {
+        Physics2D.IgnoreLayerCollision(passPlayerLayer, passGroundLayer, true);
+        gameObject.layer = passPlayerLayer;
+        yield return new WaitUntil(() => (rigid.velocity.y < 0 && !isGround));
+        Physics2D.IgnoreLayerCollision(passPlayerLayer, passGroundLayer, false);
+        gameObject.layer = playerLayer;
     }
 
     //워크카운터 코루틴
@@ -257,7 +288,7 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     private void OnTriggerExit2D(Collider2D col)
     {
         //대화창 오프 트리거
-        if(col.gameObject.tag == "NPC")
+        if (col.gameObject.tag == "NPC")
         {
             npcName = "";
             npcNum = -1;
